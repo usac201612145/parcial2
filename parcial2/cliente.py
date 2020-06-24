8b42bb5dbef043d0964f1b6a84a94e22bc4430ca
usac201602648 <3004573400101@ingenieria.usac.edu.gt>
2020-06-23 23:20:53 -0600

    Push Final

--- parcial2/cliente.py
+++ parcial2/cliente.py
@@ -12,7 +12,7 @@ import threading #JDBM Concurrencia con hilos
 import os 
 from brokerData import * 
 
-# JDBM funcion de manejo de audio 
+# JDBM funcion de manejo de audio por medio de hilo
 def audioManage(state,hora):
     message = 'aplay '+hora+'.wav'
     logging.debug("AUDIO REPRODUCIDO")
@@ -79,13 +79,16 @@ def fileRead(fileName):
 #-----------------------------------------------------------------------------------------------------------A    
 #-------------------------------------------------------------------------------------------------------------------------------------V 
 # JCAG
+#Clase para el manejo del cliente
 class ClientManagment:
     def __init__(self, user, destino,  text, fsize):
-        self.user=user
-        self.destino=destino                         #Clase para el manejo del cliente
-        self.text=text
-        self.fsize=fsize
+        self.user = user
+        self.destino = destino                         
+        self.text = text
+        self.fsize = fsize
 
+    #Funcion para publicar en el topic de salas o a un usuario, se le envia el destino(sala o carnet) 
+    # y el mensaje que se va enviar
     def ClientMessage(self):
         if len(self.destino)<8:
             client.publish("salas/15/"+str(self.destino), ' '+str(self.user)+' ('+str(self.destino)+')'+' >>>: '+str(self.text) , qos = 0, retain = False)  
@@ -93,11 +96,15 @@ class ClientManagment:
             client.publish("usuarios/15/"+str(self.destino), ' '+str(self.user)+' >>>: '+str(self.text) , qos = 0, retain = False)  
         return
 
+    #Funcion para suscribirse a un topic, se suscribe con el usuario al que se envia, esta funcion se usa cuando 
+    # se selcciona la opcion de texto
     def ClientSubsMsg(self):
         client.subscribe(("usuarios/15/"+str(self.user), qos)) 
         client.subscribe(("audio/15/"+str(self.user), qos)) 
         return
-
+    
+    #Funcion para suscribirse a las salas y al topic de audio. Esta funcion se usa cuando se selecciona en el menu
+    #la opcion de enviar audio
     def ClientSubsSalas(self):
         client.subscribe(("salas/15/"+str(self.text), qos)) 
         client.subscribe(("audio/15/"+str(self.text), qos)) 
@@ -116,6 +123,7 @@ class ClientManagment:
         return
 
 #-------------------------------------------------------------------------------------------------------------------------------------A 
+# PMJO
 # INICIO DE CLIENTE MQTT
 client = mqtt.Client(clean_session=True) 
 client.on_connect = on_connect 
@@ -126,39 +134,44 @@ client.connect(host=MQTT_HOST, port = MQTT_PORT)
 qos = 0
 
 #-----------------------------------------------------------------------------------------------------------
-                                                                                                            
+
+#Se lee el archivo de usuarios, para usar el carne que esta en el archivo                                                                                              
 subs = fileRead('usuarios')                                                                                           
 subs = subs[0]                                                                                                       
 usuario = subs[0]          
-usuario = usuario.strip()                                                                                #|JCGA
+usuario = usuario.strip()                                                                                   #|PMJO
 del subs[0]                                                                                                     
 ##Subscripcion simple con tupla (topic,qos)                                                                 #|Subscricion topics de archivo de configuracion
+
+#Se crea el objeto send de la clase ClienteManagment
 send = ClientManagment(usuario,0,0,0)
 ClientManagment.ClientSubsMsg(send)     
 
+
+#Se lee el archivo de usuarios, para usar las salas que estan en el archivo y suscribirse al topic  
 subs = fileRead('salas')                                                                                           
 subs = subs[0]  
 newsubs = []
 
+#Se toma solo el dato de la sala, por ejemplo S01 del archivo de salas
 for i in subs:
     subs = i.split("15")
     element = subs[1]
     newsubs.append(element.strip()) 
-                                                                                                                                                                                                                       
+
+#Se realiza la suscripcion a los topics de las salas usan la clase ClienteManagmen                                                                                                                                                                                                                       
 for i in newsubs:    
     send = ClientManagment(0,0,i,0)                                                                                          
     ClientManagment.ClientSubsSalas(send)                                                       
                                                                                                             
-#------------------------------------------------------------------------------------------------------------A
-
-#Iniciamos el thread (implementado en paho-mqtt) para estar atentos a mensajes en los topics subscritos
+#------------------------------------------------------------------------------------------------------------
 client.loop_start()
 #El thread de MQTT queda en el fondo, mientras en el main loop hacemos otra cosa
-
-#----------------------------------------------------------------------------------------------------------------------------V    
+#------------------------------------------------------------------------------------------------------------  
 
 try:    
-    while True:   
+    while True:     #Este codigo lo ejecutamos siempre para mantener el menu constante y seleccionar entre texto, audio
+                    # ingresar la duracion del audio, salirse del chat, etc.
         
         formato = input("(Audio/Texto)?: ")                                                                                                                                  
         destinatario = input("Destino(201612145/S00)?: ")    
@@ -169,9 +182,14 @@ try:
                 mensaje = input("Tu: ")        
                 if mensaje == 'salir':
                     pase = False     
-                else:  
+                else:
+                    #Al haber guardado los datos ingresados por el usuario, se usan los datos de usuario, destinatario y mensaje
+                    #para enviarlos a los topics ingresados y publicarlos por MQTT
                     send=ClientManagment(usuario,destinatario,mensaje,0)   
-                    ClientManagment.ClientMessage(send)    
+                    ClientManagment.ClientMessage(send)
+
+                    #JCAGA | JDBM
+                    #Si el usuario selecciono que desea enviar audio, se ejecuta este elif para que ingrese la duracion del audio
         elif formato == 'Audio' or formato == 'audio': 
             duracionIn = input("Ingrese la duracion del mensaje: ")
             send=ClientManagment(usuario,destinatario,0,0)
